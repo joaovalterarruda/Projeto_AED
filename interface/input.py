a5363ba2bb9e3925bbbb75dfd9e9263ff7fc1a80
Joao Arruda <2021110421@uac.pt>
2023-06-03 00:13:28 +0000

    submenu adicionado

--- interface/input.py
+++ interface/input.py
@@ -16,8 +16,25 @@ def opcoes_menu():
     print(" 7 - Avaliar visita")
     print(" 8 - Consultar estatísticas de visitas")
     print(" 9 - Obter sugestões de visitas")
-    print("10 - Consultar a rede de circulação")
-    print(" 0 - Sair ")
+    print("10 - Submenu Grafo")
+    print(" 0 - Sair do programa")
+
+
+def sub_menu():
+    print("\u250C" + "\u2500" * 70 + "\u2510")
+    print(
+        "\u2502" + "\033[1;36m" + "\t \t \t \t \t \tConcelho de Ponta Delgada\t \t \t \t \t   " + "\033[0m" + "\u2502")
+    print(
+        "\u2502" + "\033[1;36m" + "\t \t \t \t \t \t   Pontos de Interesse\t \t \t \t \t \t   " + "\033[0m" + "\u2502")
+    print("\u2514" + "\u2500" * 70 + "\u2518")
+    print("-" * 72)
+    print("1- Consultar a rede de circulação: ")
+    print("2- Consultar pontos críticos da vida de circullação: ")
+    print("3- Interromper via de circulação: ")
+    print("4- Obter itenerário (caminho mais curto): ")
+    print("5- Consultar rotas para percursos de carro: ")
+    print("0- Sair do submenu")
+
 
 
 
